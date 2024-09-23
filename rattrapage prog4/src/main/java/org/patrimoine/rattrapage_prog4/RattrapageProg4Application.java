c35b4a0e4e57cd63f6960156d1d8c6e46dfa0e56
ManoaRatsimandresy <hei.manoa.3@gmail.com>
2024-09-23 14:42:10 +0300

    initialize project

---
+++ rattrapage prog4/src/main/java/org/patrimoine/rattrapage_prog4/RattrapageProg4Application.java
@@ -0,0 +1,13 @@
+package org.patrimoine.rattrapage_prog4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RattrapageProg4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RattrapageProg4Application.class, args);
+    }
+
+}
---
+++ rattrapage prog4/src/test/java/org/patrimoine/rattrapage_prog4/RattrapageProg4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patrimoine.rattrapage_prog4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RattrapageProg4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
